8a0a3b0ed837ca66eb682a3a5cda56834297595b
Ihtisham Ul Haq <Ihtisham Ul Haq@gmail.com>
2024-08-10 12:02:17 +0500

    for pull

--- AfaqMobileShop/AfaqMobileShop.Server/Program.cs
+++ AfaqMobileShop/AfaqMobileShop.Server/Program.cs
@@ -6,11 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<AfaqMobileShopContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("ProdConn")));
+builder.Services.AddDbContext<AfaqMobileShopContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AfaqMobileShopContext>();
+    await dbContext.Database.MigrateAsync();
+  
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
